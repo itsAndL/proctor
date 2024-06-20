5e3572e0684d8bb8dedc3c46d9ea22fd38706bf2
itsAndL <ayoub003lkebch@gmail.com>
2024-06-20 11:28:34 +0100

    Setting up the register & login pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def svg_tag(icon_name, options = {})
+    file = Rails.root.join('app/assets/images/svg', "#{icon_name}.svg").read
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+
+    options.each { |attr, value| svg[attr.to_s] = value }
+
+    doc.to_html.html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'login', to: 'login#index'
+  get 'register', to: 'register#index'
+  get 'home', to: 'home#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
