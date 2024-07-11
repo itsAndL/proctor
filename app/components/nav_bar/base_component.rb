20b9bedf26ce29ef67dfaebfe81c0436160cd789
itsAndL <ayoub003lkebch@gmail.com>
2024-07-11 09:34:29 +0100

    Applying the GuestNavbar on guest pages

--- app/components/nav_bar/base_component.rb
+++ app/components/nav_bar/base_component.rb
@@ -1,17 +1,26 @@
 # frozen_string_literal: true
 
 class NavBar::BaseComponent < ViewComponent::Base
-  private attr_reader :user
-
-  def initialize(user)
+  def initialize(request:, user:)
+    @request = request
     @user = user
   end
 
   def component
-    if user.present?
-      NavBar::UserComponent.new(user)
-    else
+    if @user.blank? || active_guest_path
       NavBar::GuestComponent.new
+    else
+      NavBar::UserComponent.new(@user)
     end
+  end
+
+  private
+
+  def active_guest_path
+    guest_paths.include?(@request.path)
+  end
+
+  def guest_paths
+    [root_path, home_path, contact_path, about_path]
   end
 end
