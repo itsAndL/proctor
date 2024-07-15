377c70275b91d63230a31141a8bab6b1b35a1846
itsAndL <ayoub003lkebch@gmail.com>
2024-07-15 12:20:14 +0100

    Configure Hashid salt with fallback to dummy salt

--- config/initializers/hashid.rb
+++ config/initializers/hashid.rb
@@ -1,6 +1,10 @@
 Hashid::Rails.configure do |config|
   salt = Rails.application.credentials.dig(:hashid, :salt)
-  raise "Missing hashid salt in production!" if salt.blank? && Rails.env.production?
+
+  # If salt is not found in credentials, use a dummy salt
+  # This allows the application to build and run in environments where
+  # credentials are not available (e.g., during Docker build process)
+  salt ||= SecureRandom.hex
 
   config.salt = salt
   config.min_hash_length = 8
