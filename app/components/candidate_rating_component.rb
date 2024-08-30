4e2e499d89d437ddcd85a42917f94fa31e17ed55
itsAndL <ayoub003lkebch@gmail.com>
2024-08-30 16:53:07 +0100

    Conditional rendering within the candidate assessment report

--- app/components/candidate_rating_component.rb
+++ app/components/candidate_rating_component.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
-# app/components/candidate_rating_component.rb
 class CandidateRatingComponent < ViewComponent::Base
   def initialize(assessment_participation:)
     @assessment_participation = assessment_participation
+  end
+
+  def render?
+    true if @assessment_participation.started? || @assessment_participation.completed?
   end
 end
