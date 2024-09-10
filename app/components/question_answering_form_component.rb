ac03945c0664d041b9df588a61422aee00482589
abdokin <bashbin.sh@gmail.com>
2024-09-10 11:13:05 +0100

    Refactor question answering form component to use instance variables instead of session service

--- app/components/question_answering_form_component.rb
+++ app/components/question_answering_form_component.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 class QuestionAnsweringFormComponent < ViewComponent::Base
-  def initialize(session_service:, save_answer_path:)
-    @session_service = session_service
+  def initialize(business:, passed_questions_count:,questions_count:, question:, test:, is_preview:, question_started_at:, save_answer_path:)
+    @current_business = business
+    @passed_questions_count = passed_questions_count
+    @questions_count = questions_count
+    @question = question
+    @test = test
+    @preview = is_preview
     @save_answer_path = save_answer_path
+    @question_started_at = question_started_at
+  end
+
+  def calculate_duration_left
+    return unless @question_started_at
+
+    duration_left = @question.duration_seconds - (Time.zone.now - Time.zone.parse(@question_started_at))
+    duration_left.positive? ? duration_left : 0
   end
 end
