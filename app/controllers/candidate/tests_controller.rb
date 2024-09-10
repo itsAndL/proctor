01eb17d1bd3dbcef3963d42e7a37c9906a230aab
abdokin <bashbin.sh@gmail.com>
2024-09-10 11:14:05 +0100

    Refactor TestsController to include assessment participation setup logic and progress tracking

--- app/controllers/candidate/tests_controller.rb
+++ app/controllers/candidate/tests_controller.rb
@@ -1,6 +1,7 @@
 class Candidate::TestsController < ApplicationController
   before_action :authenticate_candidate!
   before_action :hide_navbar
+  before_action :set_assessment_participation
   before_action :set_participation_test_service, except: %i[feedback]
 
   def show
@@ -12,59 +13,106 @@ class Candidate::TestsController < ApplicationController
   end
 
   def start
-    @test_service.start_test(@current_test)
+    begin
+      @test_service.start_test(@current_test)
+    rescue ActiveRecord::RecordNotFound => e
+      Rails.logger.error(e)
+      redirect_to feedback_candidate_test_path(@current_test.hashid)
+    end
   end
 
   def intro
-    @test_service.start_practice_test(@current_test)
+    begin
+      @test_service.start_practice_test(@current_test)
+    rescue ActiveRecord::RecordNotFound => e
+      Rails.logger.error(e)
+      redirect_to feedback_candidate_test_path(@current_test.hashid)
+    end
   end
 
   def feedback
-    @participation_progress = ParticipationProgressService.new(session)
-    @assessment_participation = @participation_progress.assessment_participation
-    @current_test = @participation_progress.test
-    @next_test = @assessment_participation.unanswered_tests.first
-    if @next_test.present?
-      @next_url = candidate_test_path(@next_test.hashid)
-    else
-      # For now, we gone skip the custom questions
-      @next_url = checkout_candidate_assessment_participation_path(@assessment_participation.hashid)
+    begin
+      @current_test = @assessment_participation.answered_tests.last
+      redirect_to candidate_assessment_participations_path(@assessment_participation.id) unless @current_test
+      @next_test = @assessment_participation.unanswered_tests.first
+      @next_url = @next_test.present? ? candidate_test_path(@next_test.hashid) : checkout_candidate_assessment_participation_path(@assessment_participation.hashid)
+    rescue ActiveRecord::RecordNotFound => e
+      Rails.logger.error(e)
+      redirect_to candidate_assessment_participations_path(@assessment_participation.id)
     end
   end
 
   def questions
-    @test_service.start_question
-    render QuestionAnsweringFormComponent.new(
-      session_service: @participation_progress,
-      save_answer_path: save_answer_candidate_test_path(@current_test.hashid),
-    )
+    begin
+      @test_service.start_question
+      render QuestionAnsweringFormComponent.new(
+        business: @assessment_participation.assessment.business,
+        passed_questions_count: @test_service.passed_questions_count,
+        questions_count: @test_service.questions_count,
+        question: @test_service.current_question,
+        test: @current_test,
+        is_preview: @test_service.preview,
+        question_started_at: @test_service.question_started_at,
+        save_answer_path: save_answer_candidate_test_path(@current_test.hashid),
+      )
+    rescue ActiveRecord::RecordNotFound => e
+      Rails.logger.error(e)
+      redirect_to feedback_candidate_test_path(@current_test.hashid)
+    end
   end
 
   def practice_questions
-    @test_service.start_question
-    render QuestionAnsweringFormComponent.new(
-      session_service: @participation_progress,
-      save_answer_path: save_answer_candidate_test_path(@current_test.hashid),
-    )
+    begin
+      if @test_service.preview
+        @test_service.start_question_preview
+        render QuestionAnsweringFormComponent.new(
+          business: @assessment_participation.assessment.business,
+          passed_questions_count: @test_service.passed_questions_count,
+          questions_count: @test_service.questions_count,
+          question: @test_service.current_question,
+          test: @current_test,
+          is_preview: @test_service.preview,
+          question_started_at: @test_service.question_started_at,
+          save_answer_path: save_answer_candidate_test_path(@current_test.hashid),
+        )
+      else
+        redirect_to questions_candidate_test_path(@current_test.hashid)
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      Rails.logger.error(e)
+      redirect_to questions_candidate_test_path(@current_test.hashid)
+    end
   end
 
   def save_answer
-    if !@test_service.preview
-      selected_options_ids = if params[:selected_options].is_a?(Array)
-          params[:selected_options]
-        end
-    end
-    @test_service.save_answer(selected_options_ids)
-    @test_service.next_question
-    if @test_service.has_more_questions?
-      render turbo_stream: turbo_stream.replace("question-form", QuestionAnsweringFormComponent.new(
-        session_service: @participation_progress,
-        save_answer_path: save_answer_candidate_test_path(@current_test.hashid),
-      ))
-    elsif @test_service.preview
-      redirect_to start_candidate_test_path(@current_test.hashid)
-    else
-      redirect_to feedback_candidate_test_path(@current_test.hashid)
+    begin
+      if @test_service.preview
+        @test_service.next_question
+      elsif params[:selected_options].is_a?(Array)
+        @test_service.save_answer(params[:selected_options], question: @test_service.current_question)
+      else
+        @test_service.save_answer([], question: @test_service.current_question)
+      end
+
+      if @test_service.has_more_questions?
+        render turbo_stream: turbo_stream.replace("question-form", QuestionAnsweringFormComponent.new(
+          business: @assessment_participation.assessment.business,
+          passed_questions_count: @test_service.passed_questions_count,
+          questions_count: @test_service.questions_count,
+          question: @test_service.current_question,
+          test: @current_test,
+          is_preview: @test_service.preview,
+          question_started_at: @test_service.question_started_at,
+          save_answer_path: save_answer_candidate_test_path(@current_test.hashid),
+        ))
+      elsif @test_service.preview
+        redirect_to start_candidate_test_path(@current_test.hashid)
+      else
+        redirect_to feedback_candidate_test_path(@current_test.hashid)
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      Rails.logger.error(e)
+      redirect_to feedback_candidate_test_path(@current_test.hashid), alert: "Question not found."
     end
   end
 
@@ -75,9 +123,27 @@ class Candidate::TestsController < ApplicationController
   end
 
   def set_participation_test_service
-    @participation_progress = ParticipationProgressService.new(session)
-    @assessment_participation = @participation_progress.assessment_participation
+    set_assessment_participation
+    set_current_test
+    set_test_service
+  end
+
+  def set_assessment_participation
+    participation_id = session["participation_progress"]["current_assessment_participation_id"]
+    redirect_to candidate_assessment_participations_path unless participation_id
+    @assessment_participation = AssessmentParticipation.find(participation_id)
+  end
+
+  def set_current_test
     @current_test = @assessment_participation.unanswered_tests.find_by_hashid(params[:hashid])
-    @test_service = ParticipationTestService.new(session, @current_test)
+    redirect_to candidate_assessment_participation_path(@assessment_participation.id) unless @current_test
+  end
+
+  def set_test_service
+    @test_service = ParticipationTestService.new(
+      ->(key) { session["participation_progress"][key] },
+      ->(key, value) { session["participation_progress"][key] = value },
+      @current_test, @assessment_participation
+    )
   end
 end
