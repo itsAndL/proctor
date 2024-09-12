86af5c5e2b0dccda2daa2be784626f31e7f0ca9b
Abderrahman Laraich <bashbin.sh@gmail.com>
2024-09-12 13:05:56 +0000

    Better error messages

--- app/errors/participation_test_errors.rb
+++ app/errors/participation_test_errors.rb
@@ -1,19 +1,19 @@
 module ParticipationTestErrors
   class TestNotFoundError < StandardError
-    def initialize(msg = 'Test not found')
+    def initialize(msg = 'Sorry, we could not find the test you are looking for.')
       super
     end
   end
 
   class QuestionNotFoundError < StandardError
-    def initialize(test_id, question_id)
-      super("Question not found for test ID: #{test_id} and question ID: #{question_id}")
+    def initialize(msg = 'Sorry, we could not find the question you are looking for.')
+      super
     end
   end
 
   class TestQuestionNotFoundError < StandardError
-    def initialize(test_id, question_id)
-      super("TestQuestion not found for test ID: #{test_id} and question ID: #{question_id}")
+    def initialize(msg = 'Sorry, we could not find the test question you are looking for.')
+      super
     end
   end
 end
