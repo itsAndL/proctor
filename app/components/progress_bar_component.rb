b72c0a25b7aa89ab3908becba581a2a8c9cf7ff1
Abderrahman Laraich <bashbin.sh@gmail.com>
2024-09-19 14:54:55 +0000

    Fix progress bar component to handle nil total value

--- app/components/progress_bar_component.rb
+++ app/components/progress_bar_component.rb
@@ -4,7 +4,7 @@ class ProgressBarComponent < ViewComponent::Base
   def initialize(progress: 0, total: 0, classes: nil, progress_classes: '', total_classes: '', data: {})
     super
     @progress = progress
-    @total = total
+    @total = total || 0
     @classes = classes || 'mt-1 col-span-9 w-full bg-gray-200 rounded-full h-2.5'
     @progress_classes = progress_classes
     @total_classes = total_classes
@@ -12,7 +12,7 @@ class ProgressBarComponent < ViewComponent::Base
   end
 
   def percent
-    return 0 if @total.zero?
+    return 0 if @total&.zero?
 
     (@progress.to_f / @total) * 100
   end
