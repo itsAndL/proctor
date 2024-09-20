9756eb2d2da7f4135627fdb2fb55e803737bc667
Abderrahman Laraich <bashbin.sh@gmail.com>
2024-09-20 11:23:43 +0000

    Remove feedback & render with turbo in save answer

--- app/controllers/candidate/custom_questions_controller.rb
+++ app/controllers/candidate/custom_questions_controller.rb
@@ -4,29 +4,25 @@ class Candidate::CustomQuestionsController < ApplicationController
   before_action :authenticate_candidate!
   before_action :hide_navbar
   before_action :set_assessment_participation
-  before_action :set_current_custom_question, except: %i[feedback]
+  before_action :set_current_custom_question
   before_action :validate_save_answer_params, only: %i[save_answer]
 
   def start; end
 
-  def feedback
-    @current_custom_question = @assessment_participation.custom_questions.find(params[:hashid])
-    @next_url = if @participation_service.more_custom_questions?
-                  questions_candidate_custom_question_path(@participation_service.first_unanswered_custom_question)
-                else
-                  checkout_candidate_assessment_participation_path(@assessment_participation)
-                end
-  end
-
   def questions
     @participation_service.start_custom_question(@current_custom_question)
-
     render custom_question_form
   end
 
   def save_answer
     @participation_service.create_custom_question_answer(@current_custom_question, params)
-    redirect_to feedback_candidate_custom_question_path(@current_custom_question)
+    @current_custom_question = @participation_service.first_unanswered_custom_question
+    if @participation_service.more_custom_questions? && @current_custom_question.present?
+      @participation_service.start_custom_question(@current_custom_question)
+      render turbo_stream: turbo_stream.replace('question-form', custom_question_form)
+    else
+      redirect_to checkout_candidate_assessment_participation_path(@assessment_participation)
+    end
   end
 
   private
