f4200cdcc3a3143bdd6a581e59732509dda52121
abdokin <bashbin.sh@gmail.com>
2024-09-25 12:06:54 +0100

    Remove my local config

--- db/seeds/custom_questions.rb
+++ db/seeds/custom_questions.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-# gist_id = 'f04a8f3c30c4279d731372f1394a0b90'
-# custom_questions = SeedsHelper.fetch_gist_content(gist_id)
-#
-custom_questions = JSON.parse(File.read('db/seeds/data/custom_questions.json'))
+gist_id = 'f04a8f3c30c4279d731372f1394a0b90'
+custom_questions = SeedsHelper.fetch_gist_content(gist_id)
+
 custom_questions.each do |custom_question_data|
   SeedsHelper.create_custom_question!(
     custom_question_data['title'],
--- db/seeds/tests.rb
+++ db/seeds/tests.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-# gist_id = '32a7b9b7d563441f0c27bc0f2808563d'
-# tests_data = SeedsHelper.fetch_gist_content(gist_id)
-tests_data = JSON.parse(File.read('db/seeds/data/tests.json'))
+gist_id = '32a7b9b7d563441f0c27bc0f2808563d'
+tests_data = SeedsHelper.fetch_gist_content(gist_id)
 
 tests_data.each do |test_data|
   test = SeedsHelper.create_multiple_choice_test!(test_data['title'], test_data['attributes'])
