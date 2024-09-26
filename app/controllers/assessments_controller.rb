bf4cc7f91f672dcd31262e23a957ab9a92222c45
abdokin <bashbin.sh@gmail.com>
2024-09-26 14:16:36 +0100

    Remove authenticate_business!

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,5 +1,4 @@
 class AssessmentsController < ApplicationController
-  # # TODO: remove before_action :authenticate_business!
   before_action :authenticate_user!
   before_action :set_assessment, except: %i[index new create]
   before_action :authorize_record, except: %i[index new create]
