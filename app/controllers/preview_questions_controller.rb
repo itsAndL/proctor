c8101a60fc0fbddb07ab5e3877e8a34a63d33cc1
abdokin <bashbin.sh@gmail.com>
2024-09-26 16:01:10 +0100

    Secure preview questions

--- app/controllers/preview_questions_controller.rb
+++ app/controllers/preview_questions_controller.rb
@@ -1,21 +1,18 @@
 class PreviewQuestionsController < ApplicationController
-  before_action :authenticate_business!
+  before_action :authenticate_user!
   before_action :hide_navbar
 
   def show
     @test = Test.find(params[:test_library_hashid])
     @question = Question.find(params[:hashid])
-
+    authorize! @test, with: TestPolicy
     render QuestionAnsweringFormComponent.new(
       business: current_business,
       question: @question,
       test: @test,
       show_progress: false,
       monitoring: false,
-      save_path: if @question.next_preview(@test)
-                   test_library_preview_question_path(@test,
-                                                      @question.next_preview(@test))
-                 end
+      save_path: (test_library_preview_question_path(@test, @question.next_preview(@test)) if @question.next_preview(@test))
     )
   end
 end
