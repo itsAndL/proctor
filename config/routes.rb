012a4ce9931a644170b5f4a8359478d65daa1e77
abdokin <bashbin.sh@gmail.com>
2024-09-30 12:11:52 +0100

    Add Spanish & German translations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
-  scope '(:locale)', locale: /en|fr/ do
+  scope '(:locale)', locale: /en|fr|es|de/ do
     namespace :api do
       resources :monitoring, only: [:update], param: :hashid
     end
