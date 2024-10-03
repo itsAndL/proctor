dde61fb0f7c39e4dd02312d5249b24c3840436ba
abdokin <bashbin.sh@gmail.com>
2024-10-03 12:15:42 +0100

    Fix footer don't show when locale is english

--- app/components/footer_component.rb
+++ app/components/footer_component.rb
@@ -6,12 +6,17 @@ class FooterComponent < ViewComponent::Base
   end
 
   def render?
-    guest_paths.include?(@request.path)
+    guest_paths.include?(remove_locale_from_path(@request.path))
   end
 
   private
 
+  def remove_locale_from_path(path)
+    locales = I18n.available_locales.join('|')
+    path.sub(%r{^/(#{locales})(/|$)}, '/')
+  end
+
   def guest_paths
-    [root_path, home_path, contact_path, about_path]
+    [root_path, home_path, contact_path, about_path].map { |path| remove_locale_from_path(path) }
   end
 end
