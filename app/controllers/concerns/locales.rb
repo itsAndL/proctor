b64d4a30d3c4b2c09a73f339469e976c4e734933
abdokin <bashbin.sh@gmail.com>
2024-10-07 15:46:21 +0100

    use the user browser language settings

--- app/controllers/concerns/locales.rb
+++ app/controllers/concerns/locales.rb
@@ -5,18 +5,18 @@ module Locales
 
   included do
     def set_locale(&action)
+      accept_language = request.env['HTTP_ACCEPT_LANGUAGE']&.split(',')&.first(2)&.join
       locale =
-        if params[:locale].present? && I18n.available_locales.map(&:to_s).include?(params[:locale])
+        if params[:locale].present? && valide_locale?(params[:locale])
           params[:locale].to_sym
-        elsif session[:locale].present? && I18n.available_locales.map(&:to_s).include?(session[:locale])
-          session[:locale].to_sym
         elsif user_signed_in?
           current_user.locale.to_sym
+        elsif accept_language.present? && valide_locale?(accept_language)
+          accept_language.to_sym
         else
           I18n.default_locale
         end
 
-      session[:locale] = locale
       current_user.update(locale:) if user_signed_in? && current_user.locale.to_sym != locale
       if params[:locale] != locale.to_s && request.get?
         redirect_to url_for(params.permit!.merge(locale:))
@@ -31,6 +31,10 @@ module Locales
 
     def resolve_locale(locale = I18n.locale)
       locale == I18n.default_locale ? nil : locale
+    end
+
+    def valide_locale?(locale)
+      I18n.available_locales.map(&:to_s).include?(locale)
     end
   end
 end
