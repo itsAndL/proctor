b0109767b5d5db7895004b905193c8e6ece466a6
abdokin <bashbin.sh@gmail.com>
2024-10-14 10:05:51 +0100

    Remove migration to remove 'es' and 'de' languages from user locale enum

--- db/migrate/20241010102803_remove_es_and_de_from_user_local_enum.rb
+++
@@ -1,9 +0,0 @@
-class RemoveEsAndDeFromUserLocalEnum < ActiveRecord::Migration[7.1]
-  def up
-    User.where(locale: %i[es de]).update_all(locale: :fr)
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
